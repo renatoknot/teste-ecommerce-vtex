9e830db149eb7f3b7c56f40da5bd1055c1221ae0
Renato Gonçalves <renatoslip@hotmail.com>
2022-07-31 19:56:00 -0300

    slick products - ok

--- js/script.js
+++ js/script.js
@@ -20,21 +20,19 @@ $(document).ready(function () {
 const renderElements = (products) => {
   const listProducts = document.querySelector(".list-products");
 
-  console.log(products);
   products.map((product) => {
     const li = document.createElement("li");
     li.className = "product-item";
-    // console.log(product.items[0].images[0].imageUrl);
 
     const img = document.createElement("img");
     img.setAttribute("src", product.items[0].images[0].imageUrl);
     img.setAttribute("alt", product.productName);
 
-    const productName = document.createElement("span");
+    const productName = document.createElement("p");
     productName.appendChild(document.createTextNode(product.productName));
     productName.className = "product-name";
 
-    const productPrice = document.createElement("span");
+    const productPrice = document.createElement("p");
     productPrice.appendChild(
       document.createTextNode(
         product.items[0].sellers[0].commertialOffer.Price.toLocaleString(
@@ -59,3 +57,31 @@ const getProducts = () =>
     .then((products) => renderElements(products));
 
 getProducts();
+
+setTimeout(() => {
+  $(".list-products").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    arrows: true,
+    centerMode: true,
+    centerPadding: "60px",
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          // dots: true,
+          // infinite: true,
+          // arrows: true,
+          // centerMode: true,
+          // centerPadding: "60px",
+          // adaptiveHeight: true,
+        },
+      },
+    ],
+  });
+}, 1000);
